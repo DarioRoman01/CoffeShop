c555f1ebc7a97a61f346fb65013921394619e43a
Haizza1 <darioroman2001@gmail.com>
2021-06-18 16:36:23 -0400

    editing images service.

--- images/src/main.rs
+++ images/src/main.rs
@@ -1,7 +1,58 @@
-mod storage;
-mod local;
-mod handlers;
+use std::io::Write;
 
-fn main() {
-    println!("Hello, world!");
+use actix_files::NamedFile;
+use actix_multipart::Multipart;
+use actix_web::{self, get, post, web, App, Error, HttpResponse, HttpServer, Result};
+use futures::{StreamExt, TryStreamExt};
+use serde::Deserialize;
+use std::path::PathBuf;
+
+#[derive(Deserialize)]
+struct FileParams {
+    id: String,
+    filename: String,
+}
+
+#[post("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}")]
+async fn save_file(
+    mut payload: Multipart,
+    data: web::Path<FileParams>,
+) -> Result<HttpResponse, Error> {
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let filepath = format!(
+            "./imagestore/{}/{}",
+            data.id,
+            sanitize_filename::sanitize(&filename)
+        );
+
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+    }
+
+    Ok(HttpResponse::Ok().into())
+}
+
+#[get("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}")]
+async fn get_file(file_data: web::Path<FileParams>) -> Result<NamedFile> {
+    let path = PathBuf::from(format!(
+        "./imagestore/{}/{}",
+        file_data.id, file_data.filename
+    ));
+    Ok(NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_file).service(save_file))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
--- images/src/storage.rs
+++
@@ -1,5 +0,0 @@
-use std::io;
-
-pub trait Storage {
-    fn save(&self, path: String, file: &mut Box<dyn io::Read>) -> io::Result<()>;
-}
