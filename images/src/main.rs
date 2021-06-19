3ce6190f7fd7575dc6b939ac28503c46cb1cebed
Haizza1 <darioroman2001@gmail.com>
2021-06-19 16:50:32 -0400

    add cors to image service.

--- images/src/main.rs
+++ images/src/main.rs
@@ -1,8 +1,9 @@
 use std::io::Write;
 
 use actix_files::NamedFile;
+use actix_cors::Cors;
 use actix_multipart::Multipart;
-use actix_web::{self, web, App, Error, HttpResponse, HttpServer, Result};
+use actix_web::{self, web, App, Error, HttpResponse, HttpServer, Result, http::header};
 use futures::{StreamExt, TryStreamExt};
 use serde::Deserialize;
 use std::{fs, path::PathBuf};
@@ -15,18 +16,17 @@ struct FileParams {
 
 async fn save_file(
     mut payload: Multipart,
-    data: web::Path<FileParams>,
+    web::Path(id): web::Path<u32>
 ) -> Result<HttpResponse, Error> {
     while let Ok(Some(mut field)) = payload.try_next().await {
-        let filepath = format!(
-            "./imagestore/{}",
-            data.id,
-        );
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let filepath = format!("./imagestore/{}", &id);
 
         let mut path = PathBuf::from(filepath);
         if !path.exists() {
             fs::create_dir(&path)?;
-            path = path.join(PathBuf::from(&data.filename));
+            path = path.join(PathBuf::from(sanitize_filename::sanitize(&filename)));
             let mut f = web::block(|| fs::File::create(path))
                 .await
                 .unwrap();
@@ -36,7 +36,7 @@ async fn save_file(
                 f = web::block(move || f.write_all(&data).map(|_| f)).await?;
             }
         } else {
-            path = path.join(PathBuf::from(&data.filename));
+            path = path.join(PathBuf::from(sanitize_filename::sanitize(&filename)));
             match fs::remove_file(&path) {
                 Ok(()) => (),
                 Err(_) => return Ok(HttpResponse::NotFound().body("Unable to update the requested file")),
@@ -81,11 +81,20 @@ async fn get_file(file_data: web::Path<FileParams>) -> Result<NamedFile> {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| 
         App::new()
+        .wrap(Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST", "DELETE"])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600)
+        )
         .service(
             web::resource("/images/{id:[0-9]+}/{filename:[a-zA-Z]+\\.[a-z]{3}}")
-            .route(web::post().to(save_file))
             .route(web::get().to(get_file))
             .route(web::delete().to(delete_file))
+        )
+        .service(
+            web::resource("/images/{id:[0-9]+}")
+            .route(web::post().to(save_file))
         ))
         .bind("127.0.0.1:8080")?
         .run()
